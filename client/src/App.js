d8cfce6a6d381f83f9b5596c3514b168f31e3043
Anavaer <repcak95@gmail.com>
2021-11-18 17:33:50 +0100

    setup connection with the API

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { ApiService } from "./services/api-service";
 
 function App() {
+  const [sampleData, setSampleData] = useState(null)
+
+  useEffect(() => {
+    const apiService = new ApiService();
+    apiService.getSampleData().then(response => setSampleData(response.data));
+  }, [])
+
+console.log(sampleData)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Adres: {sampleData.address}</p>
+        <p>Miasto: {sampleData.city}</p>
+        <p>Nazwa firmy: {sampleData.companyName}</p>
+        <p>Imię: {sampleData.firstName}</p>
+        <p>Nazwisko: {sampleData.LastName}</p>
+        <p>NIP: {sampleData.nip}</p>
       </header>
     </div>
   );

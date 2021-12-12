e4bd815bbd0aed0a6b1d19c120d5e82c671fe855
Wiktor Dmitruk <wiktor.dmitruk@gmail.com>
2021-12-12 21:17:30 +0100

    Entities, generic UoW, generic repo added

---
+++ API/DataModel/Entities/Order.cs
@@ -0,0 +1,36 @@
+﻿using API.DataModel.Entities.AspNetIdentity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DataModel.Entities
+{
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+        [ForeignKey("User")]
+        public int ClientId { get; set; }
+        [Range(1930, 2050, ErrorMessage = "Rok musi się zawierać pomiędzy 1930 a 2050")]
+        public DateTime ServiceDate { get; set; }
+        public int OrderStatusId { get; set; }
+        [MaxLength(30)]
+        public string City { get; set; }
+        [MaxLength(75)]
+        public string Address { get; set; }
+        [Required]
+        public int Area { get; set; }
+        [ForeignKey("User")]
+        public int EmployeeId { get; set;}
+
+
+        //--------------------------------Navigation-------------------------
+        public virtual User Client{ get; set; }
+        public virtual OrderStatus OrderStatus { get; set; }
+        public virtual User Employee { get; set; }
+    }
+}
---
+++ API/DataModel/Entities/OrderToServicePrice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DataModel.Entities.AspNetIdentity
+{
+    public class OrderToServicePrice
+    {
+        [Key]
+        [Column(Order = 0)]
+        public int OrderId { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        public int ServicePriceId {get;set;}
+    }
+}

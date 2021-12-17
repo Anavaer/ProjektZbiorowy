acadba377b7569a3eb4485ca2b38f5d8b1c9aaa6
Wiktor Dmitruk <wiktor.dmitruk@gmail.com>
2021-12-17 23:20:02 +0100

    Order getters implemented, UserDto added.

---
+++ API/DTO/UserDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTO
+{
+    public class UserDto
+    {
+        public int    Id { get; set; }
+        public string CompanyName { get; set; }
+        public string NIP { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public virtual string PhoneNumber { get; set; }       
+        public virtual string Email { get; set; }
+    }
+}

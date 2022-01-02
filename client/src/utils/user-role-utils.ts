61b5d755b231634cccf277209307c43f8fd64620
tobiasz.piwowarczyk <tobiasz.piwowarczyk@outlook.com>
2022-01-02 03:11:57 +0100

    - created user role utils file for checking user roles

---
+++ client/src/utils/user-role-utils.ts
@@ -0,0 +1,9 @@
+export class UserRoleUtils {
+  public static isClient = (roles?: string[]): boolean => (roles ?? []).includes("Client");
+
+  public static isWorker = (roles?: string[]): boolean => 
+    UserRoleUtils.isClient(roles) && (roles ?? []).includes("Worker");
+
+  public static isAdmin = (roles?: string[]): boolean => 
+    UserRoleUtils.isWorker(roles) && (roles ?? []).includes("Admin");
+}

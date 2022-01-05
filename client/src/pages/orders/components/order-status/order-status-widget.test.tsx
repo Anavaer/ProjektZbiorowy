2fee0abbb87581334e95e2ea474714369f341ff6
tobiasz.piwowarczyk <tobiasz.piwowarczyk@outlook.com>
2022-01-05 17:55:21 +0100

    - created additional unit tests for order status widget

--- client/src/pages/orders/components/order-status/order-status-widget.test.tsx
+++ client/src/pages/orders/components/order-status/order-status-widget.test.tsx
@@ -1,7 +1,9 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import axios from "axios";
 import * as React from 'react';
+import { Order } from "types";
 import { OrderUtils } from "utils/order-utils";
+import { mockUserList } from "__mocks__/admin";
 import { mockOrderItem } from "__mocks__/order";
 import { mockOrderStatusList } from "__mocks__/order-status";
 import { OrderStatusWidget } from "./order-status-widget";
@@ -24,11 +26,15 @@ describe("OrderStatusWidgetTest", () => {
     mockAxios.get.mockResolvedValueOnce({
       data: mockOrderStatusList
     });
-  })
+  });
 
+  afterEach(() => {
+    mockAxios.get.mockClear();
+  });
 
+  
   test("Should render order status widget correctly", async () => {
-    render(<OrderStatusWidget value={mockOrderItem.orderStatus} completed={false} callbackBuilder={orderStatusStateChangeCallbackBuilder} />);
+    render(<OrderStatusWidget order={mockOrderItem} callbackBuilder={orderStatusStateChangeCallbackBuilder} />);
     const orderStatusWidget = await screen.findByRole("order-status-widget");
     const orderStatusWidgetButton = await screen.findByRole("order-status-widget-button");
 
@@ -45,7 +51,16 @@ describe("OrderStatusWidgetTest", () => {
 
 
   test("For not completed order, button should open menu", async () => {
-    render(<OrderStatusWidget value={mockOrderItem.orderStatus} completed={false} callbackBuilder={orderStatusStateChangeCallbackBuilder} />);
+    Object.defineProperty(document, "cookie", {
+      writable: true,
+      value: `token=test-token; id=${mockOrderItem.client.id}; role=["Client", "Worker"]`
+    });
+
+    let processedOrder: Order = Object.assign({}, mockOrderItem);
+    processedOrder.orderStatus = { "orderStatusId": 1, "description": "NEW" };
+    processedOrder.employee = mockUserList[0];
+
+    render(<OrderStatusWidget order={processedOrder} callbackBuilder={orderStatusStateChangeCallbackBuilder} />);
     const orderStatusWidgetButton = await screen.findByRole("order-status-widget-button");
 
     fireEvent.click(orderStatusWidgetButton);
@@ -56,7 +71,16 @@ describe("OrderStatusWidgetTest", () => {
 
 
   test("For completed order, button should not open menu", async () => {
-    render(<OrderStatusWidget value={mockOrderItem.orderStatus} completed={true} callbackBuilder={orderStatusStateChangeCallbackBuilder} />);
+    Object.defineProperty(document, "cookie", {
+      writable: true,
+      value: `token=test-token; id=${mockOrderItem.client.id}; role=["Client", "Worker"]`
+    });
+
+    let processedOrder: Order = Object.assign({}, mockOrderItem);
+    processedOrder.orderStatus = { "orderStatusId": 4, "description": "COMPLETED" };
+    processedOrder.employee = mockUserList[0];
+
+    render(<OrderStatusWidget order={processedOrder} callbackBuilder={orderStatusStateChangeCallbackBuilder} />);
     const orderStatusWidgetButton = await screen.findByRole("order-status-widget-button");
 
     fireEvent.click(orderStatusWidgetButton);
@@ -64,39 +88,195 @@ describe("OrderStatusWidgetTest", () => {
     const orderStatusWidgetMenu = await screen.queryByRole("order-status-widget-menu");
     expect(orderStatusWidgetMenu).toBeNull();
   });
-
+  
 
 
   
   [
+    // Logged user is a client
     {
       status: { "orderStatusId": 1, "description": "NEW" },
+      userId: mockOrderItem.client.id,
+      employee: null,
+      roles: ["Client"],
       fields: [
-        { "orderStatusId": 2, "description": "CONFIRMED" },
         { "orderStatusId": 5, "description": "CANCELED" }
       ]
     },
     {
       status: { "orderStatusId": 2, "description": "CONFIRMED" },
+      userId: mockOrderItem.client.id,
+      employee: mockUserList[0],
+      roles: ["Client"],
       fields: [
-        { "orderStatusId": 3, "description": "ONGOING" },
         { "orderStatusId": 5, "description": "CANCELED" }
       ]
     },
     {
       status: { "orderStatusId": 3, "description": "ONGOING" },
+      userId: mockOrderItem.client.id,
+      employee: mockUserList[0],
+      roles: ["Client"],
+      fields: []
+    },
+    {
+      status: { "orderStatusId": 4, "description": "COMPLETED" },
+      userId: mockOrderItem.client.id,
+      employee: mockUserList[0],
+      roles: ["Client"],
+      fields: []
+    },
+    // Logged user is worker and user is creator of order
+    {
+      status: { "orderStatusId": 1, "description": "NEW" },
+      userId: mockOrderItem.client.id,
+      employee: null,
+      roles: ["Client", "Worker"],
       fields: [
-        { "orderStatusId": 4, "description": "COMPLETED" },
         { "orderStatusId": 5, "description": "CANCELED" }
       ]
+    },
+    {
+      status: { "orderStatusId": 2, "description": "CONFIRMED" },
+      userId: mockOrderItem.client.id,
+      employee: mockUserList[0],
+      roles: ["Client", "Worker"],
+      fields: [
+        { "orderStatusId": 5, "description": "CANCELED" }
+      ]
+    },
+    {
+      status: { "orderStatusId": 3, "description": "ONGOING" },
+      userId: mockOrderItem.client.id,
+      employee: mockUserList[0],
+      roles: ["Client", "Worker"],
+      fields: []
+    },
+    {
+      status: { "orderStatusId": 4, "description": "COMPLETED" },
+      userId: mockOrderItem.client.id,
+      employee: mockUserList[0],
+      roles: ["Client", "Worker"],
+      fields: []
+    },
+    // Logged user is admin and user is creator of order
+    {
+      status: { "orderStatusId": 1, "description": "NEW" },
+      userId: mockOrderItem.client.id,
+      employee: null,
+      roles: ["Client", "Worker", "Admin"],
+      fields: [
+        { "orderStatusId": 5, "description": "CANCELED" }
+      ]
+    },
+    {
+      status: { "orderStatusId": 2, "description": "CONFIRMED" },
+      userId: mockOrderItem.client.id,
+      employee: mockUserList[0],
+      roles: ["Client", "Worker", "Admin"],
+      fields: [
+        { "orderStatusId": 5, "description": "CANCELED" }
+      ]
+    },
+    {
+      status: { "orderStatusId": 3, "description": "ONGOING" },
+      userId: mockOrderItem.client.id,
+      employee: mockUserList[0],
+      roles: ["Client", "Worker", "Admin"],
+      fields: []
+    },
+    {
+      status: { "orderStatusId": 4, "description": "COMPLETED" },
+      userId: mockOrderItem.client.id,
+      employee: mockUserList[0],
+      roles: ["Client", "Worker", "Admin"],
+      fields: []
+    },
+    // Logged user is worker and user is not order owner
+    {
+      status: { "orderStatusId": 1, "description": "NEW" },
+      userId: mockOrderItem.client.id + 1,
+      employee: null,
+      roles: ["Client", "Worker"],
+      fields: [
+        { "orderStatusId": 2, "description": "CONFIRMED" }
+      ]
+    },
+    {
+      status: { "orderStatusId": 2, "description": "CONFIRMED" },
+      userId: mockOrderItem.client.id + 1,
+      employee: mockUserList[0],
+      roles: ["Client", "Worker"],
+      fields: [
+        { "orderStatusId": 3, "description": "ONGOING" }
+      ]
+    },
+    {
+      status: { "orderStatusId": 3, "description": "ONGOING" },
+      userId: mockOrderItem.client.id + 1,
+      employee: mockUserList[0],
+      roles: ["Client", "Worker"],
+      fields: [
+        { "orderStatusId": 4, "description": "COMPLETED" }
+      ]
+    },
+    {
+      status: { "orderStatusId": 4, "description": "COMPLETED" },
+      userId: mockOrderItem.client.id + 1,
+      employee: mockUserList[0],
+      roles: ["Client", "Worker"],
+      fields: []
+    },
+    // Logged user is admin and user is not order owner
+    {
+      status: { "orderStatusId": 1, "description": "NEW" },
+      userId: mockOrderItem.client.id + 1,
+      employee: null,
+      roles: ["Client", "Worker", "Admin"],
+      fields: [
+        { "orderStatusId": 2, "description": "CONFIRMED" }
+      ]
+    },
+    {
+      status: { "orderStatusId": 2, "description": "CONFIRMED" },
+      userId: mockOrderItem.client.id + 1,
+      employee: mockUserList[0],
+      roles: ["Client", "Worker", "Admin"],
+      fields: [
+        { "orderStatusId": 3, "description": "ONGOING" }
+      ]
+    },
+    {
+      status: { "orderStatusId": 3, "description": "ONGOING" },
+      userId: mockOrderItem.client.id + 1,
+      employee: mockUserList[0],
+      roles: ["Client", "Worker", "Admin"],
+      fields: [
+        { "orderStatusId": 4, "description": "COMPLETED" }
+      ]
+    },
+    {
+      status: { "orderStatusId": 4, "description": "COMPLETED" },
+      userId: mockOrderItem.client.id + 1,
+      employee: mockUserList[0],
+      roles: ["Client", "Worker", "Admin"],
+      fields: []
     }
   ]
   .forEach(entry => {
-    test(`For status "${entry.status.description}" menu should contain fields: ${entry.fields.map(x => x.description).join(",")}`, async () => {
+    test(`For status "${entry.status.description}"; userId: ${entry.userId} and roles: [${entry.roles.join(",")}] menu should contain items: [${entry.fields.map(x => x.description).join(",")}]`, async () => {
+      Object.defineProperty(document, "cookie", {
+        writable: true,
+        value: `token=test-token; id=${entry.userId}; role=${entry.roles.join(",")}`
+      });
+
+      let processedOrder: Order = Object.assign({}, mockOrderItem);
+      processedOrder.orderStatus = entry.status;
+      processedOrder.employee = entry.employee!;
+
       render(
         <OrderStatusWidget
-          value={entry.status}
-          completed={["COMPLETED", "CANCELED"].includes(entry.status.description)}
+          order={processedOrder}
           callbackBuilder={orderStatusStateChangeCallbackBuilder}
         />
       );
@@ -105,7 +285,7 @@ describe("OrderStatusWidgetTest", () => {
 
       fireEvent.click(orderStatusWidgetButton);
 
-      const orderStatusWidgetMenuItems = await screen.getAllByRole("order-status-widget-menu-item");
+      const orderStatusWidgetMenuItems = await screen.queryAllByRole("order-status-widget-menu-item");
       expect(orderStatusWidgetMenuItems.length).toEqual(entry.fields.length);
 
       for(let i = 0; i < entry.fields.length; i++) {
@@ -120,7 +300,12 @@ describe("OrderStatusWidgetTest", () => {
 
 
   test("After menu item click, it should run callbackRunner", async () => {
-    render(<OrderStatusWidget value={mockOrderItem.orderStatus} completed={false} callbackBuilder={orderStatusStateChangeCallbackBuilder} />);
+    Object.defineProperty(document, "cookie", {
+      writable: true,
+      value: `token=test-token; role=["Client", "Worker"]`
+    });
+
+    render(<OrderStatusWidget order={mockOrderItem} callbackBuilder={orderStatusStateChangeCallbackBuilder} />);
 
     const orderStatusWidgetButton = await screen.findByRole("order-status-widget-button");
 
